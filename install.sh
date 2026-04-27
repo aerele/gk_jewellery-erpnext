2dfe9b3eebe0e6da2a251a545f220bafc063522b
Kanishkar V <163565326+Kanishkar16Vijay@users.noreply.github.com>
2026-04-27 13:12:10 +0530

    Add Redis setup configuration to install script

--- install.sh
+++ install.sh
@@ -35,6 +35,8 @@ bench get-app https://github.com/Gurukrupa-Export/gurukrupa_biometric.git --bran
 bench get-app https://github.com/Gurukrupa-Export/gurukrupa_customizations.git --branch main
 
 echo "::endgroup::"
+
+echo "::group::Redis Setup"
 bench set-config -g redis_cache redis://127.0.0.1:6379
 bench set-config -g redis_queue redis://127.0.0.1:6379
 bench set-config -g redis_socketio redis://127.0.0.1:6379
